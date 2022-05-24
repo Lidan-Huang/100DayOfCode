fdca19fb32a149a31ed1a2faa619d5084ea4bd73
Lidan-Huang <huangld0314@gmail.com>
2022-05-24 14:02:41 -0700

    added requirements, test case and pseudo code of the function remvoeKthNodeFrom End

---
+++ removeKthNodeFromEnd.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**accepts: a head of a Singly Linked List and an integer k
+ * remove the kth node from the end of the list, the removal should be done in place
+ * the input head of the linked list should remain the head of the linked list after
+ * the removal is done, if the head is the node to be removed, the function should
+ * simply mutate its value and next pointer
+ * returns: function doesn't need to return anything
+ * Assume: the input linked list always have at least k nodes
+ * each LinkedList node has an integer value and next node pointing to the next node
+ * in the list or to None/null if it's the tail of the list\
+ * 
+ * test case:
+ * head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9, k = 4;
+ * output: 0 -> 1 -> 2 -> 3 -> 4 -> 5 -> 7 -> 8 -> 9
+ * 
+ * pseudo code:
+ * loop through the LinkedList to find the tail, then count back to kth position,
+ * remove the Kth node from the LinkedList, here need to check whether Kth node
+ * is the head node or not
+ */
