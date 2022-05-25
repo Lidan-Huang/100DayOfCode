0e7f28563e26b61a2922412c6648bf72830ce13e
Lidan-Huang <huangld0314@gmail.com>
2022-05-25 15:52:15 -0700

    finished adding requirements, test case and pseudo code of function hasCycleOfLinkedList

---
+++ hasCycleOfLinkedList.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * accepts: the head of a linked list
+ * returns: true if the linked list has a cycle in it; otherwise, return false
+ * 
+ * test case:
+ * head = [3, 2, 0, -4], pos = 1; => true
+ * head = [1, 2], pos = 0 => true
+ * head = [1] pos = -1; => false;
+ * 
+ * pseudo code:
+ * make a hashtable to store the nodes;
+ * loop through the linked list, if the node already exist in hashtable, means it has
+ * cycle, and return true
+ * return false
+ * 
+ * pseudo code 2:
+ * make two pointers: fast and slow, fast moves two steps each time, and slow moves
+ * only one step, if fast or fast.next never reach null, and fast and slow nodes appeasrs 
+ * in the same position, return true
+ * return false
+ */
+
