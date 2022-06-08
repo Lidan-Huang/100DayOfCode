c6edcc154f9c6ae7d1dd7800bd6315591ed9588c
Lidan-Huang <huangld0314@gmail.com>
2022-06-07 17:39:30 -0700

    finished floodFill function

--- floodFill.js
+++ floodFill.js
@@ -12,6 +12,8 @@
  * test case:
  * image=[[1, 1, 1], [1, 1, 0], [1, 0 , 1]], sr = 1, sc = 1, newColor = 2
  * => [[2, 2, 2], [2, 2, 0], [2, 0, 1]]
+ * image=[[0, 0, 0], [0, 0, 0]], sr = 0, sc = 0, newColor = 2
+ * => [[2, 2, 2], [2, 2, 2], [2, 2, 2]]
  * 
  * pseudocode:
  * make a value to hold the start point
@@ -23,3 +25,29 @@
  * 
  * return modified image
  */
+
+function floodFill(image, sr, sc, newColor) {
+  const row = image.length;
+  const col = image[0].length;
+  //check if the starting pixel is inside of the image
+  if (sr < 0 || sr >= row || sc < 0 || sc >= col) return;
+
+  const value = image[sr][sc];
+  if (value === newColor) return image;
+
+  image[sr][sc] = newColor;
+
+  //check left direction
+  if (sc - 1 >= 0 && image[sr][sc - 1] === value) floodFill(image, sr, sc - 1, newColor);
+
+  //check right direction
+  if (sc + 1 < col && image[sr][sc + 1] === value) floodFill(image, sr, sc + 1, newColor);
+
+  //check top direction
+  if (sr - 1 >= 0 && image[sr - 1][sc] === value) floodFill(image, sr - 1, sc, newColor);
+
+  //check bottom direction
+  if (sr + 1 < row && image[sr + 1][sc] === value) floodFill(image, sr + 1, sc, newColor);
+
+  return image;
+}
