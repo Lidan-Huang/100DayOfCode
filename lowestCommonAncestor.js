91998ca8932b7cf0129ad9c107ee197ee3eaa6c6
Lidan-Huang <huangld0314@gmail.com>
2022-06-10 22:43:51 -0700

    finished lowestCommonAncestor function using recursion.

--- lowestCommonAncestor.js
+++ lowestCommonAncestor.js
@@ -13,9 +13,26 @@
  * root = [2,1], p = 2, q = 1 => 2
  * 
  * pseudocode:
- * if root is null, return null
  * if p and q are between different side of root, return root
  * if they are in the same side:
  *    check root.left /root.right with q and p, recursively
  *    if they redirect parent-child, return the parent
  */
+
+//runtime: O(logn) | space: O(1)
+function lowestCommonAncestor(root, p, q) {
+  //if p and q nodes are on different sides of root node, just return root
+  if ((root.val >= p.val && root.val <= q.val) ||
+    (root.val <= p.val && root.val >= q.val)) {
+    return root;
+  }
+  //if p and q nodes on the right side of root node
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+
+  //if p and q nodes on the left side of root node
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+}
