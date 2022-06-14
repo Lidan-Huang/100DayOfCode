56f4b065c4e416934e5f78312f31138ef1692cae
Lidan-Huang <huangld0314@gmail.com>
2022-06-13 23:01:31 -0700

    used bottom-up recursion to finish isBalancedBinaryTree function

--- isBalancedBinaryTree.js
+++ isBalancedBinaryTree.js
@@ -19,3 +19,14 @@
  * 
  */
 
+function isBalancedBinaryTree(root) {
+  function dfs(root) {
+    if (root === null) return [true, 0];
+    let left = dfs(root.left);
+    let right = dfs(root.right);
+    let balanced = left[0] && right[0] && (Math.abs(left[1] - right[1]) < 2);
+    return [balanced, 1 + Math.max(left[1], right[1])];
+  }
+
+  return dfs(root)[0];
+}
