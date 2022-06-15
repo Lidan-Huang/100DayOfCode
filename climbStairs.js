015731eb339dd4e12f757aa02cb1e21ca37457de
Lidan-Huang <huangld0314@gmail.com>
2022-06-14 23:15:00 -0700

    added requirements and test cases for climbStairs function

---
+++ climbStairs.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**accepts: an integer n (steps to reach the top)
+ * returns: distinct wanys to reach to the top
+ * each time you can either climb 1 or 2 steps
+ * 
+ * test cases:
+ * n = 2 => 2 (1step + 1step)
+ * n = 3 => 3 (1+1+1, 2+1, 1+2)
+ * 
+ * pseudocode:
+ * 
+ */
