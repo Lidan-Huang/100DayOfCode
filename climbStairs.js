a93683c687d7ee82a12f913da21e6743890f5d00
Lidan-Huang <huangld0314@gmail.com>
2022-06-15 00:19:35 -0700

    solution three using dynamic programming and similar to fibonacci number

--- climbStairs.js
+++ climbStairs.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**accepts: an integer n (steps to reach the top)
- * returns: distinct wanys to reach to the top
+ * returns: distinct ways to reach to the top
  * each time you can either climb 1 or 2 steps
  * 
  * test cases:
@@ -26,8 +26,22 @@ function climbStairs(n) {
   return climbStairs_helper(0, n);
 }
 
+
 function climbStairs2(n) {
   if (n === 1) return 1;
   if (n === 2) return 2;
   return climbStairs(n - 1) + climbStairs(n - 2);
+}
+
+//runtime: O(n) | space: O(1)
+function climbStairs3(n) {
+  if (n === 1) return 1;
+  let first = 1;
+  let second = 2;
+  for (let i = 3; i <= n; i++) {
+    let temp = second;
+    second = first + second;
+    first = temp;
+  }
+  return second;
 }
