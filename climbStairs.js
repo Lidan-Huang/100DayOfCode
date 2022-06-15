69fa7dd8996a0b6cf728a792363c7f54c01af8ef
Lidan-Huang <huangld0314@gmail.com>
2022-06-14 23:45:54 -0700

    using recursion to solve the climbStairs

--- climbStairs.js
+++ climbStairs.js
@@ -9,5 +9,19 @@
  * n = 3 => 3 (1+1+1, 2+1, 1+2)
  * 
  * pseudocode:
+ * go from number 0, two options: plus 1 or plus 2
+ * compair the result to the given number, if result bigger then given number, count0
+ * if equal, count 1,
+ * if less, continue plus 1 and plus 2, and compair to the given number again
  * 
  */
+
+//runtime: O(2^n) | space: O(n)
+function climbStairs(n) {
+  function climbStairs_helper(i, n) {
+    if (i > n) return 0;
+    if (i === n) return 1;
+    return climbStairs_helper(i + 1, n) + climbStairs_helper(i + 2, n);
+  }
+  return climbStairs_helper(0, n);
+}
