175c5c06765ddf672cc847790f5fe0f52c7f5481
Lidan-Huang <huangld0314@gmail.com>
2022-06-16 14:12:04 -0700

    added requirements, testcase and pseudocode for singleNumber function.

---
+++ singleNumber.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**accepts: an non-empty array with numbers
+ * returns: the number that only appears once, as other numbers all appear twice
+ * 
+ * test case:
+ * [2,1,2] => 1
+ * 
+ * pseudocode:
+ * make a frequency of the nums
+ * loop through the map, if the value is 1, return that key
+ */
