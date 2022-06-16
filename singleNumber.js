282a85440402a48f78fd1f163edf573da5cb0a17
Lidan-Huang <huangld0314@gmail.com>
2022-06-16 14:22:22 -0700

    finished singleNumber function and passed all the tests.

--- singleNumber.js
+++ singleNumber.js
@@ -10,3 +10,17 @@
  * make a frequency of the nums
  * loop through the map, if the value is 1, return that key
  */
+
+//runtime: O(n) | space: O(n)
+function singleNumber(nums) {
+  let freq = new Map();
+
+  for (let num of nums) {
+    let count = freq.get(num) + 1 || 1;
+    freq.set(num, count);
+  }
+
+  for (let key of freq.keys()) {
+    if (freq.get(key) === 1) return key;
+  }
+}
