6e223fb06db9c6726e475f2d185439bf77697a46
Lidan-Huang <huangld0314@gmail.com>
2022-06-19 12:01:06 -0700

    finished threeSum function

--- threeSum.js
+++ threeSum.js
@@ -26,3 +26,32 @@
  *          if equal, keep updating left pointer
  * return result
  */
+
+//runtime: O(nlogn) + O(n^2) | space: O(n)
+
+function threeSum(nums) {
+  nums.sort();
+  let result = [];
+
+  for (let i= 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum < 0) left++;
+      if (sum > 0) right--;
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+        
+        while (left < right) {
+          if (nums[left] === nums[left - 1]) left++;
+        }
+      }
+    }
+  }
+  return result;
+}
