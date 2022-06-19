bfbe4c7059f8ccc720fd2335f3c372be66d13759
Lidan-Huang <huangld0314@gmail.com>
2022-06-19 11:52:29 -0700

    added requirements, test cases and pseudocode to the threeSum function

---
+++ threeSum.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**accepts: an array of integers
+ * returns: all the triplets that sum to 0
+ * the result couldn't contain duplicate triplets
+ * 
+ * test cases:
+ * nums = [-1, 0, 1, 2, -1, -4] => [[-1, -1, 2], [-1, 0, 1]]
+ * nums = [] => []
+ * nums = [0] => []
+ * 
+ * pseudocode:
+ * sort the array nums
+ * make a variable result to hold all the triplets
+ * loop through the sorted nums from the begining i = 0
+ *    if i > 0, and nums[i] equal nums[i - 1], continue the loop
+ *    else:
+ *        two pointers: left = i+1; right = nums.length - 1
+ *        while left < right:
+ *        current sum = nums[i] + nums[left] + nums[right]
+ *        if current sum less than 0; left++
+ *        if current sum great than 0; right--
+ *        if equal, push [nums[i], nums[left], nums[right]] to result
+ *        then left++, right --
+ *        make sure left less than right, check if nums[left] equal nums[left-1]
+ *          if equal, keep updating left pointer
+ * return result
+ */
