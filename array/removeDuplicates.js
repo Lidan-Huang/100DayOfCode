9cc1fc63fa049a9fed50bd17abe8407d525713ff
Lidan-Huang <huangld0314@gmail.com>
2022-07-20 22:21:09 -0700

    finished the functionality using two pointers

--- array/removeDuplicates.js
+++ array/removeDuplicates.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/**accepts: a sorted array of integers
+/**accepts: a sorted array of integers, nums
  * returns: the length of array without duplicates
  * constraints: space complexity O(1)
  * 
@@ -19,3 +19,29 @@
  * 
  * return left + 1 (as return the length)
  */
+
+//runtime: O(N) | space: O(1)
+function removeDuplicates(nums) {
+  let left = 0;
+  if (nums.length <= 1) return nums.length;
+
+  let right = 1;
+  while (right < nums.length) {
+    if (nums[left] !== nums[right]) {
+      left++;
+      right++;
+      continue;
+    }
+
+    while (nums[left] === nums[right]) {
+      right++;
+    }
+
+    if (nums[right]) {
+      nums[left + 1] = nums[right];
+      left++;
+    }
+  }
+  return left + 1;
+}
+
