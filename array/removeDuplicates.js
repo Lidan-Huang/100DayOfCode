c96fb1a4aff59b1b23290f810bce1fe43b7f1912
Lidan-Huang <huangld0314@gmail.com>
2022-07-20 22:36:11 -0700

    added two more solutions

--- array/removeDuplicates.js
+++ array/removeDuplicates.js
@@ -45,3 +45,28 @@ function removeDuplicates(nums) {
   return left + 1;
 }
 
+
+//runtime: O(N) | space: O(1)
+function removeDuplicates2(nums) {
+  let left = 0;
+  if (nums.length === 0) return 0;
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[left] !== nums[right]) {
+      left++;
+      nums[left] = nums[right];
+    }
+  }
+  return left + 1;
+}
+
+
+//runtime: O(N^2) | space: O(1)
+function removeDuplicates3(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        while (nums[i] === nums[i + 1]) {
+            nums.splice(i + 1, 1);
+            continue;
+        }
+    }
+    return nums.length;
+  };
