e86a3a7281665d99376e38dd4293de0899beee07
Lidan-Huang <huangld0314@gmail.com>
2022-07-20 22:44:45 -0700

    using Python to solve removeDuplicates function

---
+++ array/removeDuplicates.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def removeDuplicates(nums: List[int]) -> int:
+    left: int = 0
+    if len(nums) == 0:
+        return 0
+
+    for right in range(1, len(nums)):
+        if nums[left] != nums[right]:
+            left += 1
+            nums[left] = nums[right]
+    return left + 1
