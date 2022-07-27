e7c523bf1b4ee0f3f6fd6db8e2309449732d3dfe
Lidan-Huang <huangld0314@gmail.com>
2022-07-26 17:04:26 -0700

    added js solution requirements

---
+++ array/maxProfit.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**accepts: an array prices
+ * return: maximum profit you can achieve
+ * requirements: on each day, you may decide to buy and/or sell the stock, but
+ *  only hold at most one share of the stock at any time, you can buy it then
+ *  immediately sell it on the same day.
+ * 
+ * 1 <= prices.length <= 3*10^4
+ * 0 <= prices[i] <= 10^4
+ * 
+ * test cases:
+ *  - prices = [7, 1, 5, 3, 6, 4] => 7 (4 + 3)
+ *  - prices = [1, 2, 3, 4, 5] => 4
+ *  - prices = [7, 6, 5, 4, 3, 2, 1] => 0
+ */
+
