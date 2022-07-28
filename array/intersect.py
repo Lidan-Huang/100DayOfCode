64b49dd82a78c19faef4f2c69d0b73552cbc9684
Lidan-Huang <huangld0314@gmail.com>
2022-07-28 14:50:54 -0700

    solution2 using Python:get freq of list

--- array/intersect.py
+++ array/intersect.py
@@ -1,5 +1,7 @@
-from typing import List
+from typing import List, Dict
 
+# runtime: O(nlogn) | space: o(n)
+# solution1: sort the array
 def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
     result: List[int] = []
     nums1.sort()
@@ -18,4 +20,20 @@ def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
         else:
             j += 1
     
+    return result
+
+
+# runtime: O(n) | space: o(n) + o(m)
+# solution1: sort the array
+def intersect2(nums1: List[int], nums2: List[int]) -> List[int]: 
+    freq1: Dict[int, int] = dict()
+    result: List[int] = list()
+
+    for num in nums1:
+        freq1[num] = freq1.get(num, 0) + 1
+
+    for num in nums2:
+        if num in freq1 and freq1[num] > 0:
+            result.append(num)
+            freq1[num] -= 1
     return result
