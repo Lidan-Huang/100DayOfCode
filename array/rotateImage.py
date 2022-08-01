08149ccfb6e940c7b3455a96040f00711c33b28e
Lidan-Huang <huangld0314@gmail.com>
2022-07-31 23:35:16 -0700

    fixed the bug

--- array/rotateImage.py
+++ array/rotateImage.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# runtime: O(n^2) | space: O(1)
 def rotate_image(matrix: List[List[int]]) -> None:
     """
     Accept an n x n 2D matrix representing an image, rotate the image by 
@@ -9,11 +10,13 @@ def rotate_image(matrix: List[List[int]]) -> None:
     Do not return anything, modify matrix in-place instead.
     """
 
-    left, right: int = 0, len(matrix)
+    left: int = 0
+    right: int = len(matrix) - 1
 
     while left < right:
         for i in range(right - left):
-            top, bottom: int = left, right
+            top: int = left
+            bottom: int = right
 
             # save topleft to temp
             temp: int = matrix[top][left + i]
@@ -27,6 +30,12 @@ def rotate_image(matrix: List[List[int]]) -> None:
             # move topright to bottomright
             matrix[bottom][right - i] = matrix[top + i][right]
 
+            matrix[top + i][right] = temp
+            print(matrix)
         # after moving top, bottom, left and right line, move to inner matrix
         left += 1
         right -= 1
+
+
+rotate_image([[5, 1, 9, 11], [2, 4, 8, 10],
+      [13, 3, 6, 7], [15, 14, 12, 16]])
