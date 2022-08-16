5f2671c9604ba6c77509a57a318de0255d121700
Lidan-Huang <huangld0314@gmail.com>
2022-08-16 15:00:25 -0700

    added requirement of climbing stairs

---
+++ dynamicProgramming/climbingStairs.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can 
+ * you climb to the top?
+ * 
+ * test cases:
+ * n = 1 => 1 (1 step)
+ * n = 2 => 2 (1 step + 1 step; 2 steps)
+ * n = 3 => 3 (1 step + 1 step + 1 step; 2 steps + 1 step; 1 step + 2 steps)
+ * 
+ * 
+ * pseudocode:
+ * n = 1, return 1
+ * n = 2, return 2
+ * n >= 3, return (n-1) + (n-2)
+ */
