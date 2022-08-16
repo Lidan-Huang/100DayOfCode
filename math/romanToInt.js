66a116376d42031ba72f66aec3208d13daf38517
Lidan-Huang <huangld0314@gmail.com>
2022-08-15 22:44:36 -0700

    finished romanToInt function and passed all the tests

--- math/romanToInt.js
+++ math/romanToInt.js
@@ -20,3 +20,33 @@
  * s = "LVIII" => 58
  * s = "MCMXCIV" => 1994
  */
+
+
+function romanToInt(s) {
+  const symbolValue = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  };
+
+  let right = s[s.length - 1];
+  let num = symbolValue[right];
+
+  //If left symbol value is bigger than right's value, keep adding value to num,
+  //and update right symbol; if left symbol value less than right's value, subtract
+  //left symbol value from num 
+  for (let i = s.length - 2; i >= 0; i--) {
+    if (symbolValue[s[i]] >= symbolValue[right]) {
+      num += symbolValue[s[i]];
+      right = s[i];
+    } else {
+      num -= symbolValue[s[i]];
+    }
+
+  }
+  return num;
+}
