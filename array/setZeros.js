f903d2f04c3040814a2b79137047e9542497e740
Lidan-Huang <huangld0314@gmail.com>
2022-09-14 16:02:06 -0700

    added requirements of set matrix zeroes function

---
+++ array/setZeros.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire 
+ * row and column to 0's.
+ * You must do it in place.
+ * 
+ * constraints:
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -2^31 <= matrix[i][j] <= 2^31 - 1
+ * 
+ * test cases:
+ * matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]] => [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ * matrix = [[1,1,1],[1,0,1],[1,1,1]] => [[1,0,1],[0,0,0],[1,0,1]]
+ */
