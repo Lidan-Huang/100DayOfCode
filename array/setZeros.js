0208a19c7f44c6c6e03b2f9dee98f9a04a677341
Lidan-Huang <huangld0314@gmail.com>
2022-09-14 16:24:45 -0700

    finished setZeros with runtime(n*m) and space(n+m)

--- array/setZeros.js
+++ array/setZeros.js
@@ -15,3 +15,32 @@
  * matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]] => [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
  * matrix = [[1,1,1],[1,0,1],[1,1,1]] => [[1,0,1],[0,0,0],[1,0,1]]
  */
+
+//runtime: O(m*n) | space: O(m+n)
+function setZeroes(matrix) {
+  let rows = new Set();
+  let cols = new Set();
+  const row = matrix.length;
+  const col = matrix[0].length;
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (matrix[i][j] === 0) {
+        if (!rows.has(i)) rows.add(i);
+        if (!cols.has(j)) cols.add(j);
+      }
+    }
+  }
+
+  for (let i = 0; i < row; i++) {
+    if (rows.has(i)) matrix[i] = Array.from({ length: col }).fill(0);
+  }
+
+  for (let j = 0; j < col; j++) {
+    if (cols.has(j)) {
+      for (let k = 0; k < row; k++) {
+        matrix[k][j] = 0;
+      }
+    }
+  }
+};
