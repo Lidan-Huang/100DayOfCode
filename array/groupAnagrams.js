617deaff8df44cc59742ff625f1fa8399f927600
Lidan-Huang <huangld0314@gmail.com>
2022-09-15 14:15:03 -0700

    added solution one of groupAnagrams

--- array/groupAnagrams.js
+++ array/groupAnagrams.js
@@ -16,3 +16,31 @@
  * strs = [""] => [[""]]
  * strs = ["e"] => [["e"]]
  */
+
+//solution1:runtime: o(n*m*k*logk)
+function groupAnagrams1(strs) {
+  let result = [[strs[0]]];
+
+  for (let j = 1; j < strs.length; j++) {
+    //if current string is an anagram of subarray in result, add current string
+    //to subarray, else, create a new subarray and push it to the array
+    for (let i = 0; i < result.length; i++) {
+      if (isAnagram(strs[j], result[i][0])) {
+        result[i].push(strs[j]);
+        break;
+      }
+      if (i === result.length - 1) {
+        result.push([strs[j]]);
+        break;
+      }
+    }
+  }
+
+  return result;
+};
+
+function isAnagram(str1, str2) {
+  return str1.split("").sort().join("") === str2.split("").sort().join("")
+}
+
+//solution2:
