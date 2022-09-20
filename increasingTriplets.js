dd148c6a8348028df3798532eebce56e8f69c4a1
Lidan-Huang <huangld0314@gmail.com>
2022-09-19 21:47:04 -0700

    finished increasingTriplets functionality and passed all the tests

--- increasingTriplets.js
+++ increasingTriplets.js
@@ -16,4 +16,19 @@
  * [5,4,3,2,1] => false
  */
 
-function increasingTriplets(nums) {}
+//runtime: O(n) | space: O(1)
+function increasingTriplets(nums) {
+  let first = Infinity;
+  let second = Infinity;
+
+  for (let num of nums) {
+    if (num < first) {
+      first = num;
+    } else if (num < second) {
+      second = num;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
