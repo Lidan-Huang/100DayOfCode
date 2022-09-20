858f1a170c125be73559157b0cd9be03d1a0c20f
Lidan-Huang <huangld0314@gmail.com>
2022-09-19 21:43:36 -0700

    added requirements of increasing triplets subsequence

---
+++ increasingTriplets.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * Given an integer array nums
+ * return true if there exists a triple of indices (i, j, k) such that
+ * i < j < k and nums[i] < nums[j] < nums[k]. 
+ * If no such indices exists, return false.
+ * 
+ * constraints:
+ * 1 <= nums.length <= 5 * 10^5
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * 
+ * test cases:
+ * [2,1,5,0,4,6] => true
+ * [1,2,3,4,5] => true
+ * [5,4,3,2,1] => false
+ */
+
+function increasingTriplets(nums) {}
--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -7,7 +7,7 @@
  * root=[4,2,7,1,3,6,9] => [4,7,2,9,6,3,1]
  * 
  * pseudo code:
- * base base: if the node is null, return null
+ * base case: if the node is null, return null
  * if the node is not null, exchange its two children nodes
  * and recursive repeat the above steps for node.left and node.right, until
  * there's no more nodes
