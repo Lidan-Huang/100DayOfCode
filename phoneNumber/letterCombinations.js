755fb152e16bc042b033fac63e337bf31a5139b6
Lidan-Huang <huangld0314@gmail.com>
2022-11-08 09:35:04 -0800

    added description of the letterCombinations function

---
+++ phoneNumber/letterCombinations.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * Given a string, containing digits from 2-9 inclusive, 
+ * return all possible letter combinations that the number could represent, 
+ * return the answer in any order
+ * 
+ * test cases:
+ * digits = "23" => ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * digits = "" => []
+ * digits = "2" => ["a", "b", "c"]
+ * 
+ * constraints:
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9']
+ */
+
